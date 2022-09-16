c5552174308a87dd9cbb7f1e2279344676315468
SRW <48078103+wrig0421@users.noreply.github.com>
2022-09-16 18:29:27 -0500

    dh

---
+++ STM32/stm32_workspace/fw_common/lib/current_monitor/current_monitor.c
@@ -0,0 +1,8 @@
+/*
+ * current_monitor.c
+ *
+ *  Created on: Sep 11, 2022
+ *      Author: spud
+ */
+
+
---
+++ STM32/stm32_workspace/fw_common/lib/current_monitor/current_monitor.h
@@ -0,0 +1,13 @@
+/*
+ * current_monitor.h
+ *
+ *  Created on: Sep 11, 2022
+ *      Author: spud
+ */
+
+#ifndef FW_COMMON_LIB_CURRENT_MONITOR_CURRENT_MONITOR_H_
+#define FW_COMMON_LIB_CURRENT_MONITOR_CURRENT_MONITOR_H_
+
+
+
+#endif /* FW_COMMON_LIB_CURRENT_MONITOR_CURRENT_MONITOR_H_ */
