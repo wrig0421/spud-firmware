d6d042121555e37b5b62b808f4a7b78f60840c0b
SRW <48078103+wrig0421@users.noreply.github.com>
2024-12-26 14:11:58 -0600

    sdg

--- STM32/stm32_workspace/spudglo/src/app/config.h
+++ STM32/stm32_workspace/spudglo/src/app/config.h
@@ -50,7 +50,7 @@
 //#elif defined(ENABLE_ZABINSKI_TABLE_TOP)
 //#	define STRIP_1_LENGTH	376  // bar 302// biker dude 500// MN summit 214 // HELLO SPRING STRIP 1 = 459      //492// playstation controller! - 390// vice city 432// shucmacher - 636// skull sign=200 // 151 for SWENSON SIGN /// FLYNN FARM SIGN = 166!!
 //#else
-#	define STRIP_1_LENGTH	211 // 600 is tis the season sign  // bar 302// biker dude 500// MN summit 214 // HELLO SPRING STRIP 1 = 459      //492// playstation controller! - 390// vice city 432// shucmacher - 636// skull sign=200 // 151 for SWENSON SIGN /// FLYNN FARM SIGN = 166!!
+#	define STRIP_1_LENGTH	300 // 600 is tis the season sign  // bar 302// biker dude 500// MN summit 214 // HELLO SPRING STRIP 1 = 459      //492// playstation controller! - 390// vice city 432// shucmacher - 636// skull sign=200 // 151 for SWENSON SIGN /// FLYNN FARM SIGN = 166!!
 //#endif
 //#define STRIP_1_LENGTH      374 //238    115  // bar 302// biker dude 500// MN summit 214 // HELLO SPRING STRIP 1 = 459      //492// playstation controller! - 390// vice city 432// shucmacher - 636// skull sign=200 // 151 for SWENSON SIGN /// FLYNN FARM SIGN = 166!!
 #define STRIP_2_LENGTH      0 // bar 220 // HELLO SPRING STRIP 2 = 156
