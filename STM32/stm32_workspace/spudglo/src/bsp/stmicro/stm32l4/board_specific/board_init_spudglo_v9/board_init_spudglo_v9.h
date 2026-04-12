f228a3c8179d5c7e8462996bdd33ecc3e85ce83f
SRW <48078103+wrig0421@users.noreply.github.com>
2026-04-12 18:24:04 -0500

    starting to add support for spudglo 8, 9

---
+++ STM32/stm32_workspace/spudglo/src/bsp/stmicro/stm32l4/board_specific/board_init_spudglo_v9/board_init_spudglo_v9.h
@@ -0,0 +1,10 @@
+// SRW
+#include "config.h"
+#if !defined(BOARD_INIT_SPUD_GLO_V9_H)
+#define BOARD_INIT_SPUD_GLO_V9_H
+
+void board_init_specific(void);
+void board_init_specific_power_cycle_level_shifter(void);
+void board_init_peripheral_setup(void);
+
+#endif
---
+++ STM32/stm32_workspace/spudglo/src/bsp/stmicro/stm32l4/board_specific/board_init_spudglo_v9/peripheral/uart/board_spudglo_v9_uart_config.h
@@ -0,0 +1,9 @@
+// SRW
+#include "config.h"
+
+#if !defined(BOARD_SPUDGLO_V9_UART_CONFIG_H) && defined(BOARD_SPUDGLO_V9P0)
+#define BOARD_SPUDGLO_V9_UART_CONFIG_H
+
+
+
+#endif
